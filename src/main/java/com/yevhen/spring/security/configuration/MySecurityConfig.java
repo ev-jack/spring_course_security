09a15beeb7f6b9b33d8eb7a677039060dd8b4c3d
ev-jack <jolyjeck@gmail.com>
2023-03-19 14:46:22 +0200

    moved passwords to DB and created logic to authorisation

--- src/main/java/com/yevhen/spring/security/configuration/MySecurityConfig.java
+++ src/main/java/com/yevhen/spring/security/configuration/MySecurityConfig.java
@@ -1,27 +1,49 @@
 package com.yevhen.spring.security.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.User.UserBuilder;
 
+import javax.sql.DataSource;
+
 @EnableWebSecurity
 public class MySecurityConfig extends
         WebSecurityConfigurerAdapter {
 
+    @Autowired
+    DataSource dataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        UserBuilder userBuilder = User.withDefaultPasswordEncoder();
-        auth.inMemoryAuthentication()
-                .withUser(userBuilder.username("yevhen")
-                        .password("yevhen")
-                        .roles("EMPLOYEE"))
-                .withUser(userBuilder.username("maria")
-                        .password("maria")
-                        .roles("HR"))
-                .withUser(userBuilder.username("maximus")
-                        .password("maximus")
-                        .roles("MANAGER", "HR"));
+
+        auth.jdbcAuthentication().dataSource(dataSource);
+
+//        UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+//        auth.inMemoryAuthentication()
+//                .withUser(userBuilder.username("yevhen")
+//                        .password("yevhen")
+//                        .roles("EMPLOYEE"))
+//                .withUser(userBuilder.username("maria")
+//                        .password("maria")
+//                        .roles("HR"))
+//                .withUser(userBuilder.username("maximus")
+//                        .password("maximus")
+//                        .roles("MANAGER", "HR"));
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").hasAnyRole("EMPLOYEE", "HR", "MANAGER")
+                .antMatchers("/hr_info/**").hasRole("HR")
+                .antMatchers("/manager_info/**").hasRole("MANAGER")
+                .and().formLogin().permitAll();
+
+
+
     }
 }
--- src/main/webapp/WEB-INF/view/view_for_all_employees.jsp
+++ src/main/webapp/WEB-INF/view/view_for_all_employees.jsp
@@ -1,3 +1,4 @@
+<%@ taglib prefix="security" uri="http://www.springframework.org/security/tags" %>
 <%--
   Created by IntelliJ IDEA.
   User: Евгений Науменко
@@ -15,15 +16,18 @@
     <h3>Information for all employees</h3>
     <br><br>
 
+    <security:authorize access="hasRole('HR')">
     <input type="button" value="Salary"
         onclick="window.location.href = 'hr_info'">
     Only for HR staff
     <br><br>
+    </security:authorize>
 
+    <security:authorize access="hasRole('MANAGER')">
     <input type="button" value="Performance"
            onclick="window.location.href = 'manager_info'">
     Only for managers
-
+    </security:authorize>
 
 </body>
 </html>
